cb6d18245f9cd0ef9d11051db5e7f1dc628e11c7
AsIf <iamthe.angrytoast@gmail.com>
2016-01-27 14:44:53 -0800

    add bunch of tests

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'https://rubygems.org'
+
+gem 'test-kitchen'
+gem 'server-spec'
+gem 'kitchen-ansible'
+gem 'kitchen-docker'
---
+++ test/serverspec/localhost/default_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+describe 'ansible-openssh::default' do
+
+  describe package('openssh-client') do
+    it {should be_installed.by('apt') }
+  end
+
+  describe package('openssh-server') do
+    it {should be_installed.by('apt') }
+  end
+
+  describe port(22) do
+    it { should be_listening }
+  end
+
+  describe file('/var/run/ssh') do
+    it { should be_directory }
+  end
+
+  describe file('etc/ssh/sshd_config') do
+    it { should be_writable.by('owner') }
+    it { should be_writable.by('group') }
+  end
+
+  describe file('etc/ssh/sshd_config') do
+    it { should be_mode 0644 }
+  end
+
+  describe file('/etc/ssh/ssh_config') do
+    it { should be_writable.by('owner') }
+    it { should be_writable.by('group') }
+  end
+
+   describe file('/etc/ssh/ssh_config') do
+    it { should be_mode 0644 }
+  end
+
+end
